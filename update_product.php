945850c6214d7ec87fe2e6032dcbd8f1ac624980
Samer Charaf Eddine <samer.charaf3@gmail.com>
2023-12-07 20:16:02 +0200

    Fixed a typo in the variable name

--- update_product.php
+++ update_product.php
@@ -2,7 +2,7 @@
 include("../connection.php");
 include("../jwt_verification.php");
 
-$product_id = $_POST['id_product'];
+$id_product = $_POST['id_product'];
 $name = $_POST['name'];
 $description = $_POST['description'];
 $price = $_POST['price'];
@@ -14,7 +14,7 @@ $decoded=decodeJWT();
 
 if ($decoded->user_type_id == 1) {
     $sqli = $mysqli->prepare("UPDATE products SET product_name = ?, product_description = ?, product_price = ? WHERE product_id = ? AND seller_id = ?");
-    $sqli->bind_param("ssdpps", $name, $description, $price, $product_id, $seller_id);
+    $sqli->bind_param("ssdpps", $name, $description, $price, $id_product, $seller_id);
     if ($sqli->execute()) {
         
         echo json_encode("product updated succesfully");
