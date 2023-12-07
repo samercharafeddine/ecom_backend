d7d78610cd03baed1b8b39a9618d6f4526197090
Samer Charaf Eddine <samer.charaf3@gmail.com>
2023-12-07 04:54:45 +0200

    installed firebase/JWT into the project

--- connection.php
+++ connection.php
@@ -7,7 +7,6 @@ $db_name = "ecom_db";
 $mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
 
 if ($mysqli->connect_error) {
-    die("" . $mysqli->connect_error);
+    die("Connection failed: " . $mysqli->connect_error);
 }
-
 ?>
